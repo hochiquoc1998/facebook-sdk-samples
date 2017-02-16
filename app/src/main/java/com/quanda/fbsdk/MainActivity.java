b78873cc0d871dd090ac0a123b83c335cf61055c
Dang Anh Quan <dang.anh.quan@framgia.com>
2017-02-16 13:48:37 +0700

    add-facebook-sdk

--- app/src/main/java/com/quanda/fbsdk/MainActivity.java
+++ app/src/main/java/com/quanda/fbsdk/MainActivity.java
@@ -1,13 +1,49 @@
 package com.quanda.fbsdk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvInfo;
+    private LoginButton mBtnLoginFacebook;
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCallbackManager = CallbackManager.Factory.create();
+        mTvInfo = (TextView) findViewById(R.id.tv_info);
+        mBtnLoginFacebook = (LoginButton) findViewById(R.id.btn_login_facebook);
+        mBtnLoginFacebook.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mTvInfo.setText("User ID: " + loginResult.getAccessToken().getUserId() + "\n" +
+                        "Auth Token: " + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                mTvInfo.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                mTvInfo.setText("Login attempt failed.");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
